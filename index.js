a50b7e445e65251086e9ab993e4988ff67a37b19
T U <tylerletendre98@gmail.com>
2021-04-21 19:17:03 -0400

    added a GET all songs request endpoint and a GET song by id endpoint but they both behave the same

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ app.listen(5000, function(){
     console.log('Server started. Listening on port 5000');
 });
 
+//GET all songs
 app.get('/api/songs', (req,res) => {
     let songs = repoContext.songs.findAllSongs();
     res.send(songs);
-})
+});
+
+//GET song by id
+app.get("/api/songs/:id",(req,res) => {
+    let id = req.params.id;
+    let song = repoContext.songs.findAllSongs(id);
+    res.send(song);
+});
