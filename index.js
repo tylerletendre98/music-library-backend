7b3ea469860f2860d4a207493d396f6beb3f19ac
T U <tylerletendre98@gmail.com>
2021-04-21 20:28:51 -0400

    added in all my endpoint requests and they are working

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const repoContext = require('./repository/repository-wrapper');
 const express = require('express');
+const { updateSong } = require('./repository/music-repository');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(5000, function(){
     console.log('Server started. Listening on port 5000');
@@ -13,8 +17,32 @@ app.get('/api/songs', (req,res) => {
 });
 
 //GET song by id
-app.get("/api/songs/:id",(req,res) => {
+app.get('/api/songs/:id',(req,res) => {
     let id = req.params.id;
-    let song = repoContext.songs.findAllSongs(id);
+    let song = repoContext.songs.findSongById(id);
     res.send(song);
+});
+
+//POST a new song
+app.post('/api/songs',(req,res) => {
+    let newSong = req.body;
+    let addedSong = repoContext.songs.createSong(newSong);
+    res.send(addedSong);
+});
+
+//PUT endpoint updating exisiting song
+
+app.put('/api/songs/:id', (req,res) => {
+    let id = req.params.id;
+    let songToUpdate = req.body;
+    let updatedSong = repoContext.songs.updateSong(id, songToUpdate);
+    res.send(updatedSong);
+});
+
+//DELETE endpoint delete a song
+
+app.delete('/api/songs/:id',(req,res) => {
+    let id = req.params.id;
+    let updatedDataSet = repoContext.songs.deleteSong(id);
+    res.send(updatedDataSet);
 });
